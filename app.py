a861c3cba8b3e2c3f00f73093c744845b8539155
cox512 <64932698+cox512@users.noreply.github.com>
2020-08-06 19:18:25 -0500

    Started my Flask routes. Implemented .env var

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, g
+from flask_cors import CORS
+from dotenv import load_dotenv
+load_dotenv()
+
+import models
+from resources.users import user #import the user var from users.py
+
+DEBUG = True #Allows error messages to be printed out in the server.
+PORT = 8000
+
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    g.db = models.DATABASE #Sets the db variable to equal our models Database.
+    g.db.connect()#Connects app.py to models.py database
+
+@app.after_request #Closes the connection and returns the response
+def after_request(response):
+    g.db.close()
+    return response
+
+CORS(user, origins=['http://localhost:3000'], supports_credentials=True) #Sets the front-end url, support credentials allows cookies to be set to the server
+
+app.register_blueprint(user, url_prefix='/user') #Sets the handing instructions for our routes.
+
+if __name__ == '__main__':
+    models.initialize() #Calls the initialize function from models.py and initializes the Database
+    app.run(debug=DEBUG, port=PORT)
---
+++ models.py
@@ -0,0 +1,48 @@
+from peewee import *
+import datetime
+
+DATABASE = SqliteDatabase('baggr.sqlite') #Creates our SQLite database
+
+class User(Model):
+    fname = CharField()
+    lname = CharField()
+    username = CharField(unique=True) #this will change to ForeignKeyField() I believe
+    password = CharField()
+    email = CharField(unique=True)
+    #watchlists = ForeignKeyField() #I think this is what I have to do in order to get this to reference my watchlists. BUT CAN I CREATE A LIST OFLISTS HERE?
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+class Stock(Model):  #STILL BUILDING THIS. STOCKS AND WATCHLISTS WOULD HAVE A MANY TO MANY RELATIONSHIP.
+    company_name = CharField()
+    ticker = CharField()
+    current_price = DecimalField()
+    open_price = DecimalField()
+    close_price = DecimalField()
+    day_high = DecimalField()
+    day_low = DecimalField()
+    volume = IntegerField()
+
+    class Meta:
+        database = DATABASE
+
+class Watchlist(Model): #I'D REALLY RATHER NOT HAVE A WATCHLIST MODEL. I DON'T WANT A MANY TO MANY RELATIONSHIP WITH STOCKS.
+    title = CharField()
+    user = ForeignKeyField() #A way of associating a user with a watchlist. This could help overcome our (possible) inability to use a list as a datatype in the user model.
+    class Meta:
+        database = DATABASE
+
+class WatchlistStocks(Model):
+    stock = ForeignKeyField(Stock)
+    watchlist = ForeignKeyField(Watchlist)
+
+    class Meta:
+        database = DATABASE
+
+def initialize(): #Called when initializing the app
+    DATABASE.connect() #Connect to DB
+    DATABASE.create_tables([User], safe=True) #Creates the User table
+    print("TABLES Created")
+    DATABASE.close() #Closes the connection once initialization has occurred.
