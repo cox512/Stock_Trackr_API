b9323ffab02f886b09fbaf3db4c027809c0a4271
cox512 <64932698+cox512@users.noreply.github.com>
2020-08-13 19:35:23 -0500

    Added heroku site to CORS

--- app.py
+++ app.py
@@ -33,13 +33,13 @@ def load_user(userid):
         return None
 
 #DON'T FORGET TO ADD YOUR HEROKU SITE HERE
-CORS(user, origins=['http://localhost:3000'], supports_credentials=True) #Sets the front-end url, support credentials allows cookies to be set to the server
+CORS(user, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True) #Sets the front-end url, support credentials allows cookies to be set to the server
 app.register_blueprint(user, url_prefix='/user') #Sets the handing instructions for our routes.
 
-CORS(stock, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(stock, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True)
 app.register_blueprint(stock, url_prefix='/api/v1/stocks')
 
-CORS(watchlist, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(watchlist, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True)
 app.register_blueprint(watchlist, url_prefix='/api/v1/watchlists')
 
 @app.before_request
