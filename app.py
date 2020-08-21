9063a1bc1e4d50fe337c1d51ca5896affe6175cb
cox512 <64932698+cox512@users.noreply.github.com>
2020-08-21 17:16:00 -0500

    moved before/after requests

--- app.py
+++ app.py
@@ -37,6 +37,16 @@ def load_user(userid):
     except models.DoesNotExist:
         return None
 
+#DON'T FORGET TO ADD YOUR HEROKU SITE HERE
+CORS(user, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True) #Sets the front-end url, support credentials allows cookies to be set to the server
+app.register_blueprint(user, url_prefix='/user') #Sets the handing instructions for our routes.
+
+CORS(stock, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True)
+app.register_blueprint(stock, url_prefix='/api/v1/stocks')
+
+CORS(watchlist, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True)
+app.register_blueprint(watchlist, url_prefix='/api/v1/watchlists')
+
 @app.before_request
 def before_request():
     """Connect to the db before each request"""
@@ -50,16 +60,6 @@ def after_request(response):
     print("you should see this after each request")
     g.db.close()
     return response
-
-#DON'T FORGET TO ADD YOUR HEROKU SITE HERE
-CORS(user, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True) #Sets the front-end url, support credentials allows cookies to be set to the server
-app.register_blueprint(user, url_prefix='/user') #Sets the handing instructions for our routes.
-
-CORS(stock, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True)
-app.register_blueprint(stock, url_prefix='/api/v1/stocks')
-
-CORS(watchlist, origins=['http://localhost:3000', 'https://ten-bagger.herokuapp.com'], supports_credentials=True)
-app.register_blueprint(watchlist, url_prefix='/api/v1/watchlists')
 
 if 'ON_HEROKU' in os.environ: 
     print('\non heroku!')
